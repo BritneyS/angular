b63dd5df1a854e6ea4b44c71b34a93ea1924f109
Britney Smith <brit.smith7@gmail.com>
2018-05-22 19:37:12 -0400

    env change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyBMVo000g8--ASX3qASs_l9e5kqEOlNj8o",
+    authDomain: "todolistapp-d7c33.firebaseapp.com",
+    databaseURL: "https://todolistapp-d7c33.firebaseio.com",
+    projectId: "todolistapp-d7c33",
+    storageBucket: "todolistapp-d7c33.appspot.com",
+    messagingSenderId: "839865732780"
+  }
 };
